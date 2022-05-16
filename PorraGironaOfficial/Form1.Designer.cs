1b89047c5df4aca4186ad6466c6b9f96096266f9
endrit <eenndrit12@hotmail.com>
2022-05-16 18:50:39 +0200

    CambiosEndritLogin

--- PorraGironaOfficial/Form1.Designer.cs
+++ PorraGironaOfficial/Form1.Designer.cs
@@ -29,13 +29,206 @@ namespace PorraGironaOfficial
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1Login = new System.Windows.Forms.Label();
+            this.label1user = new System.Windows.Forms.Label();
+            this.label3contraseña = new System.Windows.Forms.Label();
+            this.label2login = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.linkRecuperarContra = new System.Windows.Forms.LinkLabel();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::PorraGironaOfficial.Properties.Resources.escudo_girona;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 218);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1Login
+            // 
+            this.label1Login.AutoSize = true;
+            this.label1Login.BackColor = System.Drawing.Color.Transparent;
+            this.label1Login.Font = new System.Drawing.Font("Rockwell Nova Cond", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1Login.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1Login.Location = new System.Drawing.Point(1833, 450);
+            this.label1Login.Name = "label1Login";
+            this.label1Login.Size = new System.Drawing.Size(121, 64);
+            this.label1Login.TabIndex = 1;
+            this.label1Login.Text = "LOGIN";
+            // 
+            // label1user
+            // 
+            this.label1user.AutoSize = true;
+            this.label1user.BackColor = System.Drawing.Color.Transparent;
+            this.label1user.Font = new System.Drawing.Font("Berlin Sans FB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1user.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1user.Location = new System.Drawing.Point(727, 314);
+            this.label1user.Name = "label1user";
+            this.label1user.Size = new System.Drawing.Size(143, 35);
+            this.label1user.TabIndex = 2;
+            this.label1user.Text = "USUARIO";
+            // 
+            // label3contraseña
+            // 
+            this.label3contraseña.AutoSize = true;
+            this.label3contraseña.BackColor = System.Drawing.Color.Transparent;
+            this.label3contraseña.Font = new System.Drawing.Font("Berlin Sans FB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3contraseña.ForeColor = System.Drawing.Color.IndianRed;
+            this.label3contraseña.Location = new System.Drawing.Point(727, 403);
+            this.label3contraseña.Name = "label3contraseña";
+            this.label3contraseña.Size = new System.Drawing.Size(212, 35);
+            this.label3contraseña.TabIndex = 4;
+            this.label3contraseña.Text = "CONTRASEÑA";
+            // 
+            // label2login
+            // 
+            this.label2login.AutoSize = true;
+            this.label2login.BackColor = System.Drawing.Color.Transparent;
+            this.label2login.Font = new System.Drawing.Font("Berlin Sans FB", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2login.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2login.Location = new System.Drawing.Point(765, 80);
+            this.label2login.Name = "label2login";
+            this.label2login.Size = new System.Drawing.Size(322, 105);
+            this.label2login.TabIndex = 5;
+            this.label2login.Text = "LOGIN";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.BackColor = System.Drawing.Color.DarkRed;
+            this.txtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContraseña.Font = new System.Drawing.Font("Berlin Sans FB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.ForeColor = System.Drawing.Color.White;
+            this.txtContraseña.Location = new System.Drawing.Point(733, 441);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(330, 36);
+            this.txtContraseña.TabIndex = 2;
+            this.txtContraseña.UseSystemPasswordChar = true;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.DarkRed;
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUser.Font = new System.Drawing.Font("Berlin Sans FB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.Color.White;
+            this.txtUser.Location = new System.Drawing.Point(733, 352);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(330, 36);
+            this.txtUser.TabIndex = 1;
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.BackColor = System.Drawing.Color.Transparent;
+            this.btnAcceder.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnAcceder.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnAcceder.FlatAppearance.BorderSize = 3;
+            this.btnAcceder.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAcceder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAcceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcceder.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.ForeColor = System.Drawing.Color.LightCoral;
+            this.btnAcceder.Location = new System.Drawing.Point(733, 506);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(329, 46);
+            this.btnAcceder.TabIndex = 3;
+            this.btnAcceder.Text = "ACCEDER";
+            this.btnAcceder.UseVisualStyleBackColor = false;
+            // 
+            // linkRecuperarContra
+            // 
+            this.linkRecuperarContra.AutoSize = true;
+            this.linkRecuperarContra.BackColor = System.Drawing.Color.Transparent;
+            this.linkRecuperarContra.Font = new System.Drawing.Font("Berlin Sans FB", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkRecuperarContra.LinkColor = System.Drawing.Color.IndianRed;
+            this.linkRecuperarContra.Location = new System.Drawing.Point(789, 569);
+            this.linkRecuperarContra.Name = "linkRecuperarContra";
+            this.linkRecuperarContra.Size = new System.Drawing.Size(196, 18);
+            this.linkRecuperarContra.TabIndex = 0;
+            this.linkRecuperarContra.TabStop = true;
+            this.linkRecuperarContra.Text = "¿Has olvidado la contraseña?";
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimizar.Image = global::PorraGironaOfficial.Properties.Resources.b1a570389653c212de19c57821c5ef6f_trazo_de_puntuacion_con_guion;
+            this.btnMinimizar.Location = new System.Drawing.Point(965, 18);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(44, 23);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMinimizar.TabIndex = 10;
+            this.btnMinimizar.TabStop = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Transparent;
+            this.btnCerrar.Image = global::PorraGironaOfficial.Properties.Resources.x4;
+            this.btnCerrar.Location = new System.Drawing.Point(1019, 7);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(48, 44);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::PorraGironaOfficial.Properties.Resources.HD_wallpaper_cristhian_stuani_goal_girona_fc_joy_la_liga_uruguayan_footballers_cristhian_ricardo_stuani_curbelo_neon_lights_soccer_laliga_spain;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1079, 681);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.linkRecuperarContra);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.label2login);
+            this.Controls.Add(this.label3contraseña);
+            this.Controls.Add(this.label1user);
+            this.Controls.Add(this.label1Login);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1Login;
+        private System.Windows.Forms.Label label1user;
+        private System.Windows.Forms.Label label3contraseña;
+        private System.Windows.Forms.Label label2login;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.LinkLabel linkRecuperarContra;
+        private System.Windows.Forms.PictureBox btnMinimizar;
+        private System.Windows.Forms.PictureBox btnCerrar;
     }
 }
 
--- PorraGironaOfficial/Form1.cs
+++ PorraGironaOfficial/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace PorraGironaOfficial
 {
@@ -15,6 +16,26 @@ namespace PorraGironaOfficial
         public Form1()
         {
             InitializeComponent();
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wpara, int lparam);
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
